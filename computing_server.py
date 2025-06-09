f963dc18c14d7320905198a6e74e1820eb0061c8
Florent <florent.butery@isen-ouest.yncrea.fr>
2025-06-09 18:32:41 -0400

    Add function mean, correlation

--- computing_server.py
+++ computing_server.py
@@ -1,32 +1,8 @@
 import pickle
 import sqlite3
-import base64
 import datetime
 from typing import Any
-
 from mife.multiclient.damgard import FeDamgardMultiClient
-
-
-def adapt_datetime_iso(date_time: datetime) -> str:
-    """
-    Convert a Python datetime.datetime into a timezone-naive ISO 8601 date string.
-    >>> adapt_datetime_iso(datetime.datetime(2023, 4, 5, 6, 7, 8, 9))
-    '2023-04-05T06:07:08.000009'
-    """
-    return date_time.isoformat()
-
-
-def convert_timestamp(time_stamp: bytes) -> datetime:
-    """
-    Convert an ISO 8601 formatted bytestring to a datetime.datetime object.
-    >>> convert_timestamp(b'2023-04-05T06:07:08.000009')
-    datetime.datetime(2023, 4, 5, 6, 7, 8, 9)
-    """
-    return datetime.datetime.strptime(time_stamp.decode("utf-8"), "%Y-%m-%dT%H:%M:%S.%f")
-
-
-sqlite3.register_adapter(datetime, adapt_datetime_iso)
-sqlite3.register_converter("timestamp", convert_timestamp)
 
 class ComputingServer:
     def __init__(self, db_path='encrypted_data.db'):
@@ -43,7 +19,7 @@ class ComputingServer:
                 client_id TEXT NOT NULL,
                 tag BLOB NOT NULL,
                 ciphertext BLOB NOT NULL,
-                created_at TEXT NOT NULL,
+                created_at TIMESTAMP NOT NULL,
                 UNIQUE (client_id, tag)
             )
             ''')
@@ -77,21 +53,64 @@ class ComputingServer:
         result = FeDamgardMultiClient.decrypt(data, pk, sk, bound)
         return result
 
+    def mean(self, pk, sk, tag: bytes, bound = (0, 2000)):
+        data = self._get_data_by_tag(tag)
+        # Effectuer le déchiffrement fonctionnel
+        result = FeDamgardMultiClient.decrypt(data, pk, sk, bound)
+        return result / len(data)
+
+    def correlation(self, pk, sks: tuple[3], mean_y, yy, tag: bytes,bound = (0, 2000)):
+        data = self._get_data_by_tag(tag)
+
+        xy = FeDamgardMultiClient.decrypt(data, pk, sks[0], bound)
+        xx = FeDamgardMultiClient.decrypt(data, pk, sks[1], bound)
+        sum = FeDamgardMultiClient.decrypt(data, pk, sks[2], bound)
+
+        m = len(data)
+
+        mean_x = sum / m
+        numerator = xy - (m * mean_x * mean_y)
+        denominator = ((xx - m * mean_x**2) * (yy - m * mean_y**2))**0.5
+
+        return numerator / denominator
+
+
 # Exemple d'utilisation
 if __name__ == "__main__":
-    key = FeDamgardMultiClient.generate(2, 3)
+    # Test apply fe key
+    n = 2
+    m = 3
+    key = FeDamgardMultiClient.generate(n, m)
     server = ComputingServer()
     tag = b'tag'
+
     d = FeDamgardMultiClient.encrypt([1, 2, 3], tag, key.get_enc_key(0), key.get_public_key())
     server.save_data('client123', tag, pickle.dumps(d))
     d = FeDamgardMultiClient.encrypt([4, 5, 6], tag, key.get_enc_key(1), key.get_public_key())
     server.save_data('client124', tag, pickle.dumps(d))
 
-    y = [[1 for j in range(3)] for i in range(2)] #SOMME
+    y = [[1 for j in range(m)] for i in range(n)] #SOMME
     sk = FeDamgardMultiClient.keygen(y, key)
     print(server.apply_fe_key(key.get_public_key(), sk, tag))
-    # data = server._get_data_by_tag(tag)
-    # for client_id, ciphertext, created_at in data:
-    #     print(f"{client_id} - {created_at} - {ciphertext}")
+
+    # Test Mean
+    n = 4
+    m = 1
+    key = FeDamgardMultiClient.generate(n, m)
+    server = ComputingServer()
+    tag = b'tag2'
+
+    d = FeDamgardMultiClient.encrypt([1], tag, key.get_enc_key(0), key.get_public_key())
+    server.save_data('client123', tag, pickle.dumps(d))
+    d = FeDamgardMultiClient.encrypt([2], tag, key.get_enc_key(1), key.get_public_key())
+    server.save_data('client124', tag, pickle.dumps(d))
+    d = FeDamgardMultiClient.encrypt([3], tag, key.get_enc_key(2), key.get_public_key())
+    server.save_data('client125', tag, pickle.dumps(d))
+    d = FeDamgardMultiClient.encrypt([4], tag, key.get_enc_key(3), key.get_public_key())
+    server.save_data('client126', tag, pickle.dumps(d))
+
+    y = [[1 for j in range(m)] for i in range(n)] #SOMME
+    sk = FeDamgardMultiClient.keygen(y, key)
+    print(server.mean(key.get_public_key(), sk, tag))
 
     server.close()
